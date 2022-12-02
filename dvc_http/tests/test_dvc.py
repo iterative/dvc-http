437a416a2e1b80d017e0fd8d26fbc3c9e279ae3f
Daniele Trifirò <daniele@iterative.ai>
2022-12-02 13:07:06 +0100

    cruft update

--- dvc_http/tests/test_dvc.py
+++ dvc_http/tests/test_dvc.py
@@ -5,7 +5,16 @@ from dvc.testing.api_tests import (  # noqa, pylint: disable=unused-import
 from dvc.testing.remote_tests import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
+    TestAdd as _TestAdd,
+)
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
+    TestGetUrl,
+)
 from dvc.testing.workspace_tests import TestImport as _TestImport
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
+    TestLsUrl,
+)
 
 
 @pytest.fixture
